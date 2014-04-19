6a90f6cef9b1c023fc041101102c1f588f10572c
ComboZhc <zhangchao6865@gmail.com>
2014-04-19 11:09:40 +0800

    Categorize:

--- 414_first_test/proc_test1.m
+++ 414_first_test/proc_test1.m
@@ -1,32 +1,37 @@
-function [result] = proc_test1()
+function [result, error] = proc_test1()
 
 X = csvread('test1.csv', 1);
 Y = [grpstats(X(:,[3 4]), X(:,[5 6 1 2]), 'mean') unique(X(:,[5 6 1 2]), 'rows')];
 Y = Y(Y(:,6)~=2,:);
 ap_position = [0 3; 3.9 0; 0 0; 3.9 3];
+ra=-35:2.5:-25;
+rb=6:0.5:8;
+rp=-10:0.5:30;
+rq=-20:0.5:20;
 n = size(ap_position, 1);
 m = size(Y, 1) / n;
-result = zeros(m, 5);
+result = zeros(m, 6);
 for i=1:m
     y = [Y(n*i-n+1:n*i,1:4) ap_position];
     max_value = 0;
     max_answer = [0 0 0];
-    for a=-40:-25
-        for b=6:8
+    for a=ra
+        for b=rb
             r = exp((a - y(:, 1)) / b);
-            for p=-5:0.5:20
-                for q=-10:0.5:10
-                    curr_value = pdf([p q], ap_position, r, 2.5);
+            for p=rp
+                for q=rq
+                    curr_value = pdf([p q], ap_position, r, 5);
                     if (curr_value > max_value)
                         max_value = curr_value;
-                        max_answer = [a p q];
+                        max_answer = [a b p q];
                     end
                 end
             end
         end
     end
     result(i,:) = [max_answer Y(n*i,3:4)];
-% method for intersection of circle
+ 
+% Intersection of circle method
 %     point = zeros(1, 2);
 %     for a=-37:0.2:-25
 %         c = [ap_position exp((a - y(:, 1))/b)];
@@ -40,6 +45,7 @@ for i=1:m
 %     result(i,2:3)=point;
 %     result(i,4:5)=Y(n*i,3:4);
 end
+error = sqrt(sum((result(:,3:4) - result(:,5:6)) .^ 2, 2));
 
 end
 
--- db.py
+++ db.py
@@ -4,8 +4,8 @@ __all__ = ['RadarDB']
 
 SELECT_ADDRESS_ID = 'SELECT id FROM address WHERE address = ?'
 INSERT_ADDRESS = 'INSERT INTO address(address) VALUES (?)'
-SELECT_INFO = 'SELECT id FROM info WHERE cid = ? AND aid = ? AND date = ?'
-INSERT_INFO = 'INSERT INTO info(cid, aid, rssi, snr, date, tag, x, y) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+SELECT_INFO = 'SELECT id FROM info WHERE client = ? AND ap = ? AND date = ?'
+INSERT_INFO = 'INSERT INTO info(client, ap, rssi, snr, date, tag, x, y) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
 
 class RadarDB:
     def connect(self, db_file, x=0, y=0, tag=None):
@@ -16,26 +16,10 @@ class RadarDB:
         self.tag = tag
 
     def add_info(self, client, ap, rssi, snr, date):
-        self.c.execute(SELECT_ADDRESS_ID, (client,))
-        r = self.c.fetchone()
-        if not r:
-            self.c.execute(INSERT_ADDRESS, (client,))
-            self.c.execute(SELECT_ADDRESS_ID, (client,))
-            r = self.c.fetchone()
-        cid = r[0]
-        
-        self.c.execute(SELECT_ADDRESS_ID, (ap,))
-        r = self.c.fetchone()
-        if not r:
-            self.c.execute(INSERT_ADDRESS, (ap,))
-            self.c.execute(SELECT_ADDRESS_ID, (ap,))
-            r = self.c.fetchone()
-        aid = r[0]
-
-        self.c.execute(SELECT_INFO, (cid, aid, date))
+        self.c.execute(SELECT_INFO, (client, ap, date))
         if not self.c.fetchone():
-            print client, ap, rssi, snr, date, cid, aid
-            self.c.execute(INSERT_INFO, (cid, aid, rssi, snr, date, self.tag, self.x, self.y))
+            print client, ap, rssi, snr, date
+            self.c.execute(INSERT_INFO, (client, ap, rssi, snr, date, self.tag, self.x, self.y))
             
     def commit(self):
         self.conn.commit()
